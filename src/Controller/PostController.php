9f5718800ecaf73f47305edfdc28476f0eb60024
Cristian Sena <cristianjsena@gmail.com>
2023-05-09 09:14:03 -0300

    Agrego ep para editar post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Form\PostType;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/crear', name: 'post_create')]
+    #[Route('/post/crear', name: 'post_create', methods: ['GET', 'POST'])]
     public function create(Request $request, EntityManagerInterface $entityManger ): Response
     {
         $form = $this->createForm(PostType::class);
@@ -25,6 +26,29 @@ class PostController extends AbstractController
 
             $this->addFlash('success', 'Publicación guardada con éxito');
             return $this->redirectToRoute('post_create');
+        }
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/{id}/editar', name: 'post_edit', methods: ['GET', 'POST'])]
+    public function edit(Post $post, Request $request, EntityManagerInterface $entityManger ): Response
+    {
+        //dd($post);
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+           // $entityManger->persist($form->getData()); linea opcional
+            $entityManger->flush();
+
+            $this->addFlash('success', 'Publicación editada con éxito');
+            return $this->redirectToRoute('post_edit', [
+                'id' => $post->getId()
+            ]);
         }
 
         return $this->render('post/create.html.twig', [
